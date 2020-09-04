2442773ca84cb30b5eddaeae9a8aaa1b278c53fd
benmbrew <benmbrew@gmail.com>
2020-09-04 18:01:31 -0400

    added app template

---
+++ app.R
@@ -0,0 +1,62 @@
+library(shiny)
+library(shinydashboard)
+library(sparkline)
+library(jsonlite)
+library(dplyr)
+
+header <- dashboardHeader(title="Databrew app")
+sidebar <- dashboardSidebar(
+  sidebarMenu(
+    menuItem(
+      text="Main",
+      tabName="main",
+      icon=icon("eye")),
+    menuItem(
+      text = 'About',
+      tabName = 'about',
+      icon = icon("cog", lib = "glyphicon"))
+  )
+)
+
+body <- dashboardBody(
+  tags$head(
+    tags$link(rel = "stylesheet", type = "text/css", href = "custom.css")
+  ),
+  tabItems(
+    tabItem(
+      tabName="main",
+      fluidPage(
+      
+      )
+    ),
+    tabItem(
+      tabName = 'about',
+      fluidPage(
+        fluidRow(
+          div(img(src='logo_clear.png', align = "center"), style="text-align: center;"),
+                 h4('Built in partnership with ',
+                   a(href = 'http://databrew.cc',
+                     target='_blank', 'Databrew'),
+                   align = 'center'),
+          p('Empowering research and analysis through collaborative data science.', align = 'center'),
+          div(a(actionButton(inputId = "email", label = "info@databrew.cc", 
+                             icon = icon("envelope", lib = "font-awesome")),
+                href="mailto:info@databrew.cc",
+                align = 'center')), 
+          style = 'text-align:center;'
+          )
+        )
+    )
+  )
+)
+
+# UI
+ui <- dashboardPage(header, sidebar, body, skin="blue")
+
+# Server
+server <- function(input, output) {
+  
+  
+}
+
+shinyApp(ui, server)
---
+++ global.R
@@ -0,0 +1,4 @@
+library(tidyverse)
+
+# read in data
+dat <- read.csv('Gun Violence Impact Survey  (Responses) - Form responses 1.csv')
